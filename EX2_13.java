0a181208ac8b763db9de87ba53618b93eb0247e0
dewqwer <natcha.1@mail.kmutt.ac.th>
2021-04-16 10:00:30 +0700

    เพิ่มไฟล์ EX2_13

---
+++ EX2_13.java
@@ -0,0 +1,24 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+/*
+
+
+
+
+*/
+
+class EX2_13 {
+    public static void main(String[] args) {
+        System.out.print("Enter the monthly saving amount:");
+        Scanner sc=new Scanner();
+        =sc.nextInt();
+
+
+
+
+
+        System.out.print("After the sixth month, the account value is $");
+
+    }
+
+}
